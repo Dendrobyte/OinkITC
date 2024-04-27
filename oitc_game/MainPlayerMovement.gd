00f04249b58af71ec30f82a92b63e801c5349355
Dendrobyte <markbacon78@gmail.com>
2024-04-27 16:39:40 -0700

    first person model moving

--- oitc_game/MainPlayerMovement.gd
+++ oitc_game/MainPlayerMovement.gd
@@ -6,7 +6,15 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var max_speed = 8
+var mouse_sensitivity = 0.002
 
+@onready
+var camera = $CameraPivot/PlayerView
+
+func _ready():
+	# THIS SHOULD NOT BE IN THE MAIN PLAYER MOVEMENT SCRIPT LMAO
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,12 +22,12 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -29,3 +37,10 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event):
+	# Handle camera/model movement
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$CameraPivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$CameraPivot.rotation.x = clamp($CameraPivot.rotation.x, -1.2, 1.2)

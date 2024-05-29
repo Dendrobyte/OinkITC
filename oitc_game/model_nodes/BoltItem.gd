19c97a6206298b7c342384b7705cf6cd2d914ec9
Dendrobyte <markbacon78@gmail.com>
2024-05-29 15:49:19 -0700

    Collision communicating w server and tracking!

--- oitc_game/model_nodes/BoltItem.gd
+++ oitc_game/model_nodes/BoltItem.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 var firing_speed
+var player_id
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,15 +14,16 @@ func _process(delta):
 	if collision:
 		# Spawn new static bolt?
 		var dummy = collision.get_collider(0)
-		if dummy.name == "DummyStaticBody3D":
+		if "DummyParent" in dummy.name: # Better way to compare?
 			 # TODO: I don't like this, better to have an Area3D?
-			dummy.is_hit()
+			dummy.is_hit(player_id)
 			queue_free()
 
-func fire(speed: int):
+func fire(speed: int, p_id: int):
 	# TODO: Back is heavier, this function should apply the force to the front, not center of mass?
 	set_gravity_scale(1)
 	firing_speed = speed
 	$ArrowShootSound.play()
+	player_id = p_id
 
 # TODO: signal when player model is entered, and if the bullet is in the group (make that group!) then remove this object, TODO to call a score made function?
--- oitc_game/scenes/DummyScript.gd
+++ oitc_game/scenes/DummyScript.gd
@@ -1,7 +1,9 @@
 extends StaticBody3D
 
+signal dummy_is_hit(s_id: int, v_id: int)
 
-# Called when the node enters the scene tree for the first time.
+const d_id = 122
+
 func _ready():
 	pass # Replace with function body.
 
@@ -10,7 +12,7 @@ func _ready():
 func _process(_delta):
 	pass
 
-func is_hit():
+func is_hit(p_id: int):
 	$SoundPlayer.play()
 	var newStaticBolt = load("res://assets/StaticBolt.glb").instantiate()
 	add_child(newStaticBolt)
@@ -18,4 +20,12 @@ func is_hit():
 	newStaticBolt.global_basis = global_basis
 	newStaticBolt.scale = Vector3(0.64, 0.64, 0.64) # cri
 
-	# TODO (start here!) send signal to server
+	# NOTE: Ideally this gets moved into a util function or something?
+	dummy_is_hit.emit(p_id, d_id)
+	die()
+
+func die():
+	# "Kills" the dummy
+	# TODO: Pick a random location and respawn a dummy
+	# TODO: Drop bacon
+	queue_free()
